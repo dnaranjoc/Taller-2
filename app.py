ba59faef508eb7532d60af5bc820cc232985d0ff
dnaranjoc <d.naranjoc@uniandes.edu.co>
2025-10-19 18:55:42 -0500

    Cambio app.py para subir a github

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # Función para cargar los datos de energia y transformarlos en DF
+    # Función para cargar los datos de energia y transformarlos en DF .....
     df = pd.read_csv("datos_energia.csv")
     df['time'] = pd.to_datetime(df['time'])
     df.set_index('time', inplace=True)
